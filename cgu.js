6dd1de6ebe7b1bd4482537a582d36a9be65b2bdd
alexandrerafales <92779990+alexandrerafales@users.noreply.github.com>
2022-01-19 09:41:53 +0100

    Add files via upload

---
+++ cgu.js
@@ -0,0 +1,42 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+    // -----cgu-part------
+    document.getElementById('cgu_date').textContent = id.cgu_date;
+    document.getElementById('adresse_site').textContent = id.adresse_site;
+    
+    // -----Article1-part------
+    document.getElementById('adresse_site2').textContent = id.adresse_site;
+    document.getElementById('editeur_dev1').textContent = id.editeur_dev1;
+    document.getElementById('editeur_dev2').textContent = id.editeur_dev2;
+    document.getElementById('editeur_dev3').textContent = id.editeur_dev3;
+    document.getElementById('adresse_dev').textContent = id.adresse_dev;
+    document.getElementById('tel_dev').textContent = id.tel_dev;
+    document.getElementById('mail_dev').textContent = id.mail_dev;
+    document.getElementById('adresse_site3').textContent = id.adresse_site;
+    document.getElementById('nom_hebergeur').textContent = id.nom_hebergeur;
+    document.getElementById('siege_hebergeur').textContent = id.siege_hebergeur;
+    document.getElementById('tel_hebergeur').textContent = id.tel_hebergeur;
+    
+    // -----Article2-part------
+    document.getElementById('adresse_site4').textContent = id.adresse_site;
+    document.getElementById('adresse_site5').textContent = id.adresse_site;
+    document.getElementById('services_articles').textContent = id.services_articles;
+    document.getElementById('services_site').textContent = id.services_site;
+    
+   
+   // -----Article3-part------
+    document.getElementById('numéro_cnil').textContent = id.numéro_cnil;
+    document.getElementById('mail_retrataction').textContent = id.mail_retrataction;
+    
+    // -----Article5-part------
+    document.getElementById('adresse_site6').textContent = id.adresse_site;
+    document.getElementById('adresse_site7').textContent = id.adresse_site;
+    document.getElementById('adresse_site8').textContent = id.adresse_site;
+    
+    // -----Article6-part------
+    document.getElementById('adresse_site9').textContent = id.adresse_site;
+    
+    // -----Article7-part------
+    document.getElementById('adresse_site10').textContent = id.adresse_site;
+    document.getElementById('adresse_site11').textContent = id.adresse_site;
+    
+});
